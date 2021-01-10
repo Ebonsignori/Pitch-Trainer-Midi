32fc962fc69753fcf776b29b0b15d5e4f73c358a
Evan Bonsignori <evanabonsignori@gmail.com>
2021-01-10 18:14:32 -0500

    Init boilerplate with webpack, react, electron, and eslint-standard

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12
+  },
+  plugins: [
+    'react'
+  ],
+  settings: {
+    react: {
+      version: '17'
+    }
+  },
+  rules: {
+    'comma-dangle': ['error', {
+      arrays: 'only-multiline',
+      objects: 'only-multiline',
+      imports: 'only-multiline',
+      exports: 'only-multiline',
+      functions: 'never'
+    }]
+  }
+}
---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: orange;
+`
+
+function App () {
+  return (
+    <div>
+    <Title>Here!</Title>
+      <p>We are here!</p>
+    </div>
+  )
+}
+
+export default App
